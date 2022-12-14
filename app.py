ba682ab07febb61bbb34de8534cc137a42253a77
acemark1 <jeyachandranajinath@gmail.com>
2022-12-14 02:27:38 +0000

    View all the data is implemented.

--- app.py
+++ app.py
@@ -2,22 +2,22 @@ from flask import Flask, render_template
 import json 
 from mypyfiles.jsonHandler import JSONHandler
 
+FILE_PATH = "data/addressbook.json"
+
 app = Flask(__name__) 
-JSON_DATA_HANDLE = JSONHandler()
+JSON_DATA_HANDLE = JSONHandler(FILE_PATH)
 
-
+#Home page: View All Data from the json file.
 @app.route("/")
 def index(): 
-    json_data = JSON_DATA_HANDLE.returnJSONDataFromJSONFile()
-    print(json_data)
+    books_data = JSON_DATA_HANDLE.returnBookClassData()
+    print(books_data[0].getISBNNo())
+    return render_template("index.html", booksData = books_data)
 
 
-    
 
 
-    
 
-    return render_template("index.html")
 
 
 if __name__ == "__main__": 
--- mypyfiles/jsonHandler.py
+++ mypyfiles/jsonHandler.py
@@ -1,12 +1,24 @@
 import json 
+from mypyfiles.book import Book
+
 
 class JSONHandler():
-    def __init__(self): 
-        self.FILE_PATH = "data/addressbook.json"
+    def __init__(self, file_path:str): 
+        self.FILE_PATH = file_path
 
-    #
-    def returnJSONDataFromJSONFile(self,): 
+    #Directly go through the FILE_PATH and returns json data. 
+    def returnRawJsonData(self,): 
         file = open(self.FILE_PATH)
-        address_book_data = json.load(file) 
+        address_book_data = json.load(file)
         return address_book_data
+
+    #Get the json response from func: returnRawJsonData() and create instance of Book Object with it. And store it in an array 
+    def returnBookClassData(self,): 
+        json_data = self.returnRawJsonData()
+        book_data = [] 
+        for data in json_data: 
+            book = Book(data["id"], data["isbn_no"], data["book_name"], data["author_name"])
+            book_data.append(book)
+
+        return book_data
 
