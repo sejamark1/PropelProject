7eba56777e5c3de52bebdbda7d61eaa2147c8421
acemark1 <jeyachandranajinath@gmail.com>
2022-12-14 03:10:40 +0000

    Deletes the data from the JSON file

--- app.py
+++ app.py
@@ -5,17 +5,23 @@ from mypyfiles.jsonHandler import JSONHandler
 FILE_PATH = "data/addressbook.json"
 
 app = Flask(__name__) 
-JSON_DATA_HANDLE = JSONHandler(FILE_PATH)
+jsonDataHandler = JSONHandler(FILE_PATH)
 
 #Home page: View All Data from the json file.
 @app.route("/")
 def index(): 
-    books_data = JSON_DATA_HANDLE.returnBookClassData()
+    books_data = jsonDataHandler.returnBookClassData()
     print(books_data[0].getISBNNo())
     return render_template("index.html", booksData = books_data)
 
 
 
+
+@app.route("/delete/<int:id>") 
+def delete_data_from_json_file(id): 
+    jsonDataHandler.deleteJSONDataWithGivenId(id)
+    books_data = jsonDataHandler.returnBookClassData()
+    return render_template("index.html", booksData = books_data)
 
 
 
--- mypyfiles/jsonHandler.py
+++ mypyfiles/jsonHandler.py
@@ -6,13 +6,26 @@ class JSONHandler():
     def __init__(self, file_path:str): 
         self.FILE_PATH = file_path
 
+
+    """
+    RETURN: Array of JSON Data
+    """
     #Directly go through the FILE_PATH and returns json data. 
-    def returnRawJsonData(self,): 
+    def returnRawJsonData(self): 
         file = open(self.FILE_PATH)
         address_book_data = json.load(file)
+        file.close()
         return address_book_data
 
-    #Get the json response from func: returnRawJsonData() and create instance of Book Object with it. And store it in an array 
+    def __insert_data_into_json_file(self, json_array_data, json_file_path):
+        with open(json_file_path, "w") as file: 
+            json.dump(json_array_data, file, indent=4)
+
+
+    """
+    RETURN: array of Book.class 
+    Get the json response from func: returnRawJsonData() and 
+    create instance of Book Object with it. And store it in an array"""
     def returnBookClassData(self,): 
         json_data = self.returnRawJsonData()
         book_data = [] 
@@ -21,4 +34,21 @@ class JSONHandler():
             book_data.append(book)
 
         return book_data
+
+
+    """
+    DELETE: 
+    """
+    def deleteJSONDataWithGivenId(self, delete_id):
+        fetched_json_data = self.returnRawJsonData()
+        del_id = delete_id
+        update_entry_after_delete = [] 
+        for data in fetched_json_data: 
+            if(data["id"] == del_id): 
+                pass 
+            else: 
+                update_entry_after_delete.append(data) 
+        self.__insert_data_into_json_file(update_entry_after_delete, self.FILE_PATH)
+
+
 
